dbdb5bf3bd569b0aa892c84b3d827d16609a25c3
20093293 <20093293@mail.wit.ie>
2022-03-11 13:31:27 +0000

    Skeleton code for menus working

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import utils.ScannerInput
 import java.lang.System.exit
 import java.util.*
 
@@ -5,10 +6,8 @@ fun main(args: Array<String>) {
     runMenu()
 }
 
-    val scanner = Scanner(System.`in`)
-
 fun mainMenu() : Int {
-    print(""" 
+    return ScannerInput.readNextInt(""" 
          > ----------------------------------
          > |        NOTE KEEPER APP         |
          > ----------------------------------
@@ -21,7 +20,6 @@ fun mainMenu() : Int {
          > |   0) Exit                      |
          > ----------------------------------
          > ==>> """.trimMargin(">"))
-    return scanner.nextInt()
 }
 
     fun runMenu() {
